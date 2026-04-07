6c7080db26cd5128e438f442c92606355c2b71ca
Arthur Melkonyan <45238571+artmelkon@users.noreply.github.com>
2026-04-06 20:25:32 -0700

    refactor: improve Button component type definitions and formatting

--- components/ui/Button/index.tsx
+++ components/ui/Button/index.tsx
@@ -1,11 +1,28 @@
-import Link from 'next/link';
-import classes from './Button.module.css';
+import Link from "next/link";
+import classes from "./Button.module.css";
 
-const Button = (props: any) => {
-  if(props.url) {
-    return <Link href={props.url} className={classes.btn}>{props.children}</Link>
+type ButtonProps =
+  | { url: string; onClick?: never; children: React.ReactNode }
+  | {
+      url?: never;
+      onClick?: () => void;
+      children: React.ReactNode;
+      disabled: boolean;
+    };
+
+const Button: React.FC<ButtonProps> = (props) => {
+  if (props.url) {
+    return (
+      <Link href={props.url} className={classes.btn}>
+        {props.children}
+      </Link>
+    );
   }
-  return <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
-}
+  return (
+    <button type="button" className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
